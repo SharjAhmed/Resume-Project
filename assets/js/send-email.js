08d3a4a48f9cc806b10fb3225381ac420320e76a
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-06-07 23:14:35 +0000

    Add email functionality

---
+++ assets/js/send-email.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "project-request", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        });
+}
